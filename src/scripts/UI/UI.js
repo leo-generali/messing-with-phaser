6b0d0d4d81adc9811dd7909cc2a37413130809f4
Leo Generali <leo-generali@leos-mbp.lan>
2019-11-03 18:58:26 -0500

    Add Preact to serve as UI layer for game

---
+++ src/scripts/UI/UI.js
@@ -0,0 +1,9 @@
+import { h } from "preact";
+
+const style = {
+  position: "absolute"
+};
+
+export const UI = () => {
+  return <div style={style}>Lives: 3</div>;
+};
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,21 +1,14 @@
-import Phaser from "phaser";
-import { Play } from "./scenes";
+import { h, render, Fragment } from "preact";
+import { Game } from "./UI/Game";
+import { UI } from "./UI/UI";
 
-const config = {
-  type: Phaser.AUTO, // Which renderer to use
-  width: 160, // Canvas width in pixels
-  height: 160, // Canvas height in pixels
-  pixelArt: true,
-  zoom: 3,
-  parent: "game-container", // ID of the DOM element to add the canvas to
-  scene: [Play],
-  physics: {
-    arcade: {
-      gravity: { y: 500 },
-      debug: true
-    },
-    default: "arcade"
-  }
+const App = () => {
+  return (
+    <Fragment>
+      <UI />
+      <Game />
+    </Fragment>
+  );
 };
 
-new Phaser.Game(config);
+render(<App />, document.body);
